28f01bb8fbd42844e3d8c65ceed6d325cf04da7f
MazElena <tecttest743@gmail.com>
2023-04-17 16:32:29 +0300

    2 commit

--- Test_Git.js
+++ Test_Git.js
@@ -1,6 +1,7 @@
 let createPhoneNumber = ([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
 let newNumber = createPhoneNumber.toString().replace(/,/g,'')
 
+
 let first = newNumber.slice(0,3)
 let second = newNumber.slice(3,6)
 let third = newNumber.slice(6,10)
